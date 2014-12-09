b8a6ab6b789839ce2f3d6fcc63e63198f1545f2b
wegdrasil <wegdrasil@gmail.com>
2014-12-09 17:38:46 +0100

    Final commit

--- game.js
+++ game.js
@@ -7,14 +7,18 @@ var score = 0;
 var scoreString = '';
 var scoreText;
 var lives = 3;
+var livesText;
+var stateText;
 
 var player;
 var invaders;
 var invaderkilled_sfx;
+var playerkilled_sfx;
 
 var kaboom;
 
 var rocket = {"ph_object": null, "sfx":null, "isStillGoing": false};
+var enemyBullets;
 
 var starfield;
 
@@ -29,13 +33,14 @@ function preload()
 	game.load.image("rocket", "sprites/rocket.png");
 	game.load.audio("shoot_sfx", "sounds/shoot.wav");
 	game.load.audio("invaderkilled_sfx", "sounds/invaderkilled.wav");
-
+	game.load.audio("playerkilled_sfx", "sounds/explosion.wav");
+	
 
 	game.load.spritesheet("invader0", "sprites/invader0.png", 32, 32);
 	game.load.spritesheet("invader1", "sprites/invader1.png", 32, 32);
 	game.load.spritesheet("invader2", "sprites/invader2.png", 32, 32);
 	game.load.spritesheet("kaboom", "sprites/kaboom.png", 32, 32);
-
+	game.load.spritesheet("bullet", "sprites/enemyBullet.png", 32, 32);
 	game.load.image("starfield", "sprites/starfield.png");
 	
 	game.stage.smoothed = false;
@@ -49,12 +54,15 @@ function create()
 
 	starfield = game.add.tileSprite(0, 0, 800, 600, "starfield");
     
-    //  The score
-    scoreString = 'Score : ';
-    scoreText = game.add.text(10, 10, scoreString + score, { font: '10px Arial', fill: '#fff' });
 
-    //  Lives
-    game.add.text(game.world.width - 100, 10, 'Lives : ', { font: '10px Arial', fill: '#fff' });
+    scoreString = 'Score : ';
+    scoreText = game.add.text(10, 10, scoreString + score, { font: '16px Arial', fill: '#fff' });
+
+    livesText = game.add.text(game.world.width - 100, 10, 'Lives : ' + lives, { font: '16px Arial', fill: '#fff' });
+
+	stateText = game.add.text(game.world.centerX,game.world.centerY,' ', { font: '32px Arial', fill: '#fff' });
+	stateText.anchor.setTo(0.5, 0.5);
+	stateText.visible = false;
 
 	player = game.add.sprite(375, 550, "ship");
 	game.physics.arcade.enable(player);
@@ -63,7 +71,6 @@ function create()
 	rocket.ph_object = game.add.sprite(387, 550, "rocket");
 	game.physics.arcade.enable(rocket.ph_object);
 	rocket.ph_object.body.collideWorldBounds = true;
-	//rocket.ph_object.visible = false;
 	rocket.sfx = game.add.audio('shoot_sfx');
     rocket.sfx.allowMultiple = true;
 
@@ -76,34 +83,72 @@ function create()
     	var invader = invaders.create((i * 50)+64, 32, "invader2");
     	invader.animations.add("inv-a1", [0,1], 5, true);
     	invader.play("inv-a1");
+    	invader.outOfBoundsKill = true;
+    	invader.checkWorldBounds = true;
+    	invader.events.onOutOfBounds.add( killOnBoundary, this );
 
     	var invader = invaders.create((i * 50)+64, 64, "invader2");
     	invader.animations.add("inv-a1", [0,1], 5, true);
     	invader.play("inv-a1");
 
+		invader.outOfBoundsKill = true;
+    	invader.checkWorldBounds = true;
+    	invader.events.onOutOfBounds.add( killOnBoundary, this );
+
     	var invader = invaders.create((i * 50)+64, 96, "invader1");
     	invader.animations.add("inv-a2", [0,1], 5, true);
     	invader.play("inv-a2");
 
+		invader.outOfBoundsKill = true;
+    	invader.checkWorldBounds = true;
+    	invader.events.onOutOfBounds.add( killOnBoundary, this );
+
+
     	var invader = invaders.create((i * 50)+64, 128, "invader1");
     	invader.animations.add("inv-a2", [0,1], 5, true);
     	invader.play("inv-a2");
+		invader.outOfBoundsKill = true;
+    	invader.checkWorldBounds = true;
+    	invader.events.onOutOfBounds.add( killOnBoundary, this );
+
 
     	var invader = invaders.create((i * 50)+64, 160, "invader0");
     	invader.animations.add("inv-a3", [0,1], 5, true);
     	invader.play("inv-a3");
+		invader.outOfBoundsKill = true;
+    	invader.checkWorldBounds = true;
+    	invader.events.onOutOfBounds.add( killOnBoundary, this );
+
 
     	var invader = invaders.create((i * 50)+64, 192, "invader0");
     	invader.animations.add("inv-a3", [0,1], 5, true);
     	invader.play("inv-a3");
+		invader.outOfBoundsKill = true;
+    	invader.checkWorldBounds = true;
+    	invader.events.onOutOfBounds.add( killOnBoundary, this );
 
     }
 
     var tween = game.add.tween(invaders).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
-    tween.onLoop.add(function descend(){invaders.y += 10;}, this);
+    tween.onLoop.add(function descend(){invaders.y += 20;}, this);
+
    
     invaderkilled_sfx = game.add.audio("invaderkilled_sfx");
     invaderkilled_sfx.allowMultiple = true;
+
+    playerkilled_sfx = game.add.audio("playerkilled_sfx");
+    playerkilled_sfx.allowMultiple = true;
+
+    enemyBullets = game.add.group();
+	enemyBullets.enableBody = true;
+	enemyBullets.physicsBodyType = Phaser.Physics.ARCADE;
+	enemyBullets.createMultiple(60, "bullet");
+	enemyBullets.setAll('anchor.x', 0.5);
+	enemyBullets.setAll('anchor.y', 1);
+	enemyBullets.setAll('outOfBoundsKill', true);
+	enemyBullets.setAll('checkWorldBounds', true);
+	enemyBullets.callAll('animations.add', 'animations', 'bullet-anim', [0, 1], 5, true);
+    enemyBullets.callAll('animations.play', 'animations', 'bullet-anim');
 
 }
 
@@ -114,45 +159,55 @@ function update()
 	player.body.velocity.x = 0;
 	rocket.ph_object.body.velocity.x = 0;
 	
-	if(cursors.left.isDown)	{
-		player.body.velocity.x = -200;
-		if(!rocket.isStillGoing)
-			rocket.ph_object.body.velocity.x = -200;
-	}
-	if(cursors.right.isDown) {
-		player.body.velocity.x = +200;	
-		if(!rocket.isStillGoing)
-			rocket.ph_object.body.velocity.x = +200;
-	}
-	if(fireButton.isDown && !rocket.isStillGoing)
-	{
-		rocket.ph_object.body.velocity.y = -1000;
-		rocket.ph_object.position.x = player.position.x + 12;
-		rocket.ph_object.position.y = player.position.y + 16;
-		rocket.isStillGoing = true;
-		//rocket.ph_object.visible = true;
-		rocket.sfx.play();
-	}
-	if(rocket.ph_object.body.blocked.up)
-	{
-		console.log("batman");
-		rocket.ph_object.body.velocity.y = 0;
-		rocket.ph_object.position.x = player.position.x + 12;
-		rocket.ph_object.position.y = player.position.y + 16;
-		rocket.isStillGoing = false;	
-		//rocket.ph_object.visible = false;
+	if (player.alive)
+	{	
+		if(cursors.left.isDown)	{
+			player.body.velocity.x = -200;
+			if(!rocket.isStillGoing)
+				rocket.ph_object.body.velocity.x = -200;
+		}
+		if(cursors.right.isDown) {
+			player.body.velocity.x = +200;	
+			if(!rocket.isStillGoing)
+				rocket.ph_object.body.velocity.x = +200;
+		}
+		if(fireButton.isDown && !rocket.isStillGoing)
+		{
+			rocket.ph_object.body.velocity.y = -1000;
+			rocket.ph_object.position.x = player.world.x + 12;
+			rocket.ph_object.position.y = player.world.y + 16;
+			rocket.isStillGoing = true;
+			rocket.sfx.play();
+		}
+		if(rocket.ph_object.body.blocked.up)
+		{
+			console.log("batman");
+			rocket.ph_object.body.velocity.y = 0;
+			rocket.ph_object.position.x = player.world.x + 12;
+			rocket.ph_object.position.y = player.world.y + 16;
+			rocket.isStillGoing = false;	
+		}
+		
+		if(game.rnd.frac() > 0.95)
+		{
+			var enemyBullet = enemyBullets.getFirstExists(false);
+			if (enemyBullet)
+					invaders.forEachAlive( function (invader) {
+					if(game.rnd.frac() > 0.99)
+					{
+						enemyBullet.reset(invader.world.x, invader.world.y);
+						game.physics.arcade.moveToXY(enemyBullet, invader.world.x,invader.world.y + 1000,200);
+					}
+
+				}
+				);
+		}
+		if(!game.physics.arcade.overlap(player, invaders, killPlayerByInvader, null, this))
+			game.physics.arcade.overlap(rocket.ph_object, invaders, killInvader, null,this );
+
+		game.physics.arcade.overlap(enemyBullets, player, killPlayerByBullet, null, this);
 	}
 
-	game.physics.arcade.overlap(rocket.ph_object, invaders, killInvader, null,this );
-
-	if(enter.isDown)
-	{
-		player.position.x = 0;
-		player.position.y = 0;
-
-		rocket.ph_object.position.x = 0;
-		rocket.ph_object.position.y = 0;
-	}
 }
 
 function render() {
@@ -169,8 +224,8 @@ function killInvader(r, invader)
 
 	rocket.ph_object.body.velocity.y = 0;
 	 
-	rocket.ph_object.position.x = player.position.x + 12;
-	rocket.ph_object.position.y = player.position.y + 16;
+	rocket.ph_object.position.x = player.world.x + 12;
+	rocket.ph_object.position.y = player.world.y + 16;
 
 	rocket.isStillGoing = false;	
 
@@ -184,4 +239,96 @@ function killInvader(r, invader)
 
 	score += 1;
 	scoreText.text = scoreString + score;
+
+	if (invaders.countLiving() == 0)
+	{
+	 	stateText.text = "Click to start new level";
+		stateText.visible = true;
+		game.input.onTap.addOnce(restart,this);
+	}
+
+}
+
+function killOnBoundary(invader)
+{
+	console.log(invaders.countLiving());
+	if (invaders.countLiving() == 1)
+	{
+		console.log("does it even work?");
+	 	stateText.text = "Click to start new level";
+		stateText.visible = true;
+		game.input.onTap.addOnce(restart,this);
+	}
+}
+
+function killPlayerByBullet(player, bullet)
+{
+	playerkilled_sfx.play();
+	bullet.kill();
+	lives = lives - 1;
+	livesText.text = "Lives : " + lives;
+
+	if(lives < 1)
+	{
+		score = 0;
+
+		player.kill();
+		rocket.ph_object.kill();
+		invaders.callAll("kill");
+
+		enemyBullets.callAll("kill");
+		
+		kaboom = game.add.sprite(player.world.x, player.world.y, "kaboom");
+		var anim = kaboom.animations.add("kaboom-anim", [ 0, 1, 2, 3 ], 15, false);
+		anim.killOnComplete = true;
+
+		kaboom.animations.play("kaboom-anim");
+
+		stateText.text=" GAME OVER \n Click to restart";
+		stateText.visible = true;
+		game.input.onTap.addOnce(restart,this);
+	}
+}
+
+function killPlayerByInvader(player, invader)
+{
+	playerkilled_sfx.play();
+	
+	lives = 0;
+	livesText.text = "Lives : " + lives;
+
+	score = 0;
+
+
+	enemyBullets.callAll("kill");
+	
+	kaboom = game.add.sprite(player.world.x, player.world.y, "kaboom");
+	var anim = kaboom.animations.add("kaboom-anim", [ 0, 1, 2, 3 ], 15, false);
+	anim.killOnComplete = true;
+
+	kaboom.animations.play("kaboom-anim");
+	player.kill();
+
+	rocket.ph_object.kill();
+	invaders.callAll("kill");
+	stateText.text=" GAME OVER \n Click to restart";
+	stateText.visible = true;
+	game.input.onTap.addOnce(restart,this);
+}
+
+function restart ()
+{
+	invaders.callAll('revive');
+	invaders.x = 64;
+	invaders.y = 32;
+
+	player.revive();
+	if(lives < 1)
+	{
+		lives = 3;
+		livesText.text = "Lives : " + lives;
+	}
+	scoreText.text = scoreString + score;
+	rocket.ph_object.revive();
+	stateText.visible = false;
 }
